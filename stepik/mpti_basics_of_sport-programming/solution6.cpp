d01d1b592767f3a75554d90a9092d6d4cb276aaa
Данила Корабельников <129096056+W1NS3@users.noreply.github.com>
2023-12-25 19:28:07 +0300

    Create solution6.cpp

---
+++ stepik/mpti_basics_of_sport-programming/solution6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int j = n-1; j >= 0; j--) {
+        for (int i = n-1; i >= 0; i--) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
